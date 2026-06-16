da756d59fe20d008b1c93e6532470976b480087d
Clerk-Zar <ghaizar.bautista013@gmail.com>
2026-06-16 15:41:41 +0800

    Basta

--- app/routes/mobile.py
+++ app/routes/mobile.py
@@ -1,11 +1,12 @@
-from app.models import CTMS1000,CTMS4100, User, Module, Role
-from sqlalchemy import or_, and_
+from app.models import CTMS1000,CTMS4100, User, Module, Role, Cases
+from sqlalchemy import or_, and_, cast, Integer, func
 from app.routes.decorators import remmberToken
 from flask import Blueprint, jsonify, request, url_for
 from werkzeug.security import check_password_hash
 from secrets import token_urlsafe
 from app import db
 from sqlalchemy.orm import joinedload
+
 
 
 mobile_bp = Blueprint('apis', __name__, url_prefix='/api')
@@ -148,9 +149,77 @@ def getCR():
                     "CASENUM": case.CASENUM,
                     "CASETITLE": case.CASETITLE,
                     "DTFILED": case.DTFILED,
-                    "NATUREREM": case.NATUREREM
+           
                 }
                 for case in cases
+            ]
+        }), 200
+
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "message": str(e),
+            "data": []
+        }), 500
+
+
+@mobile_bp.route('/cases/oca')
+@remmberToken
+def getOC():
+
+    try:
+        CourtRecordsCivil = (
+            Cases.query
+            .filter(func.lower(Cases.case_type) == "civil case")
+            .order_by(Cases.id.desc())
+            .all()
+        )
+
+        return jsonify({
+            "success": True,
+            "message": "Cases retrieved successfully.",
+            "data": [
+                {
+                    "CASEID": case.id,
+                    "CASENUM": case.case_number,
+                    "CASETITLE": case.title,
+                    "DTFILED": case.date_filed.strftime("%Y-%m-%d") if case.date_filed else None,
+                }
+                for case in CourtRecordsCivil
+            ]
+        }), 200
+
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "message": str(e),
+            "data": []
+        }), 500
+
+
+@mobile_bp.route('/cases/scc')
+@remmberToken
+def getSC():
+
+    try:
+        CourtRecordsCivil = (
+            Cases.query
+            .filter(func.lower(Cases.case_type) == "smallclaims")
+            .order_by(Cases.id.desc())
+            .all()
+        )
+
+        return jsonify({
+            "success": True,
+            "message": "Cases retrieved successfully.",
+            "data": [
+                {
+                    "CASEID": case.id,
+                    "CASENUM": case.case_number,
+                    "CASETITLE": case.title,
+                    "DTFILED": case.date_filed.strftime("%Y-%m-%d") if case.date_filed else None,
+                }
+                for case in CourtRecordsCivil
             ]
         }), 200
 
